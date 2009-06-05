cc59280b7251fc13a4cb437f903ebf86c823fd62
Harry Bock <harry@oshean.org>
2009-06-04 23:15:57 -0400

    Update test Lisp driver for new structure and enumeration definitions.

--- lisp/test.lisp
+++ lisp/test.lisp
@@ -19,23 +19,36 @@
 (in-package :periscope)
 
 (defvar *flows* 0)
+(defvar *ipv4* 0)
 
 (defcallback receive-flow :void ((collector periscope-collector)
 				 (type :uchar)
 				 (record :pointer)
 				 (dsrs periscope-dsrs))
-  (declare (ignore collector record dsrs type))
+  (declare (ignore collector record))
+  (case (foreign-enum-keyword 'argus-flow-types type)
+    (:ipv4
+     (incf *ipv4*)
+     (let ((ip (get-ip (foreign-slot-value dsrs 'periscope-dsrs 'flow))))
+       (with-foreign-slots ((ip-src ip-dst ip-proto) ip argus-ip-flow)
+	 (case (foreign-enum-keyword 'ip-protocols ip-proto)
+	   (:icmp (format t "ICMP!~%"))
+	   (:tcp (format t "TCP!~%"))
+	   (:udp (format t "UDP!~%"))))))
+    (:ipv6 (format t "IPV6!~%")))
+  
   (incf *flows*))
 
 (defun test-argus ()
   (let ((collector (make-instance 'collector)))
     (with-collector-callbacks (process_flow) collector
       (setf process_flow (callback receive-flow)))
-    (setf *flows* 0)
+    (setf *flows* 0
+	  *ipv4* 0)
     (add-file collector "argus.1")
     (start collector)
     (stop collector)
-    (format t "Handled ~a flows!~%" *flows*)
+    (format t "Handled ~a flows (~a IPv4)!~%" *flows* *ipv4*)
     1))
 
 
