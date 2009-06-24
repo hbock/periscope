5d5b6728b5c5617fea3bde42c56bdae0af1b8eff
Harry Bock <harry@oshean.org>
2009-06-24 16:38:21 -0400

    Tidy up sources manager.
    Make it look like the other configuration pages.  Add some small functionality for
    removing a source by ArgusInput pointer address (not too smart, I know, but it works).

--- lisp/web-config.lisp
+++ lisp/web-config.lisp
@@ -22,7 +22,7 @@
 
 (hunchentoot:define-easy-handler (sources :uri "/sources") ()
   (with-periscope-page ("Manage Argus Sources")
-    (:h2 "Manage Argus Sources")
+    (:div :class "config-header" "Collector Operation")
     (unless *collector*
       (warning-box
        "Collector not initialized. This is a bug.")
@@ -35,21 +35,30 @@
 	   (:p "Please add one or more sources below before starting the collector."))
 	  (warning-box
 	   "The collector is not running." (:br)
-	   (:b (:a :href "/config?action=run" "Click here to run the collector.")))))
+	   (:b (:a :href "/manage-sources?action=run" "Click here to run the collector.")))))
     
     (when *collector*
       (print-remote-sources *collector*))
     
     (:p
-     (:h2 "Sources Configuration")
-     (:h3 "Add a source")
-     (:form
-      :action "/set-config" :method "post"
-      (:label :for "source" "Hostname: ")
-      (:input :type "text" :size 20 :name "source")
-      (:input :type "hidden" :name "action" :value "add")
-      (:input :type "submit" :value "Add")
-      (:br)))))
+     (:form      
+      :action "/manage-sources" :method "post"
+      (:div :class "config-header" "Add Argus Server")
+      (:div
+       :class "config-section"
+       (:table
+	(:tr
+	 (:td "Hostname")
+	 (:td (input "hostname" "")))
+	(:tr
+	 (:td "Port")
+	 (:td (input "hostname" 561)))
+	(:tr
+	 (:td "SASL Authentication")
+	 (:td (checkbox "sasl"))))
+       (:input :type "hidden" :name "action" :value "add")
+       (:input :type "submit" :value "Add")
+       (:br))))))
 
 (hunchentoot:define-easy-handler (config :uri "/config") ((err :parameter-type 'integer))
   (with-periscope-page ("Control Panel")
@@ -93,14 +102,7 @@
       (:input :type "submit" :value "Apply Configuration")))))
 
 (hunchentoot:define-easy-handler (set-config :uri "/set-config")
-    (action source (web-port :parameter-type 'integer) ports filter)
-  (when (and (not (running-p *collector*))
-	     (string= action "run"))
-    (web-run-collector *collector*))
-  (cond
-    ((string= action "add")
-     (add-remote *collector* source)))
-
+    (action (web-port :parameter-type 'integer) ports filter)
   ;; TODO: Restart server!
   (when web-port
     (setf *web-port* web-port))
@@ -108,25 +110,51 @@
   (save-config)
   (hunchentoot:redirect "/config?error=0"))
 
+(hunchentoot:define-easy-handler (manage-sources :uri "/manage-sources")
+    (action hostname port (sid :parameter-type 'integer))
+  (when (and (not (running-p *collector*))
+	     (string= action "run"))
+    (web-run-collector *collector*))
+  (cond
+    ((string= action "add")
+     (add-remote *collector* hostname))
+    ((string= action "remove")
+     (when sid
+       (setf (remote-sources *collector*)
+	     (delete-if (lambda (src)
+			  (= (pointer-address (get-ptr src)) sid))
+			(remote-sources *collector*))))))
+
+  ;;(save-config)
+  (hunchentoot:redirect "/sources?error=0"))
+
 (defun print-remote-sources (&optional (collector *collector*))
   (when (remote-sources collector)
     (with-html-output (*standard-output*)
-      (:table
-       :class "sources"
-       (:tr (:th :colspan 3 "Remote Sources"))
-       (:tr (:th "Hostname") (:th "Status") (:th "Options"))	 
-       (dolist (source (remote-sources collector))
-	 (htm
-	  (:tr (:td (str (source-path source)))
-	       (:td (str
-		     (if (connected-p source)
-			 "Connected"
-			 "Not Connected")))
-	       (:td
-		(:a :href (format nil "/config?action=remove&source=~a" (source-path source))
-		    "Remove") " "
-		(:a :href (format nil "/config?action=connect&source=~a" (source-path source))
-		    (str
-		     (if (connected-p source)
-			 "Disconnect"
-			 "Connect")))))))))))
+      (:div :class "config-header" "Manage Sources")
+      (:div
+       :class "config-section"
+       (:table
+	:class "sources"
+	(:tr (:th "Hostname") (:th "Port") (:th "Status") (:th "Options"))	 
+	(dolist (source (remote-sources collector))
+	  (htm
+	   (:tr
+	    (:td (str (source-path source)))
+	    ;; TODO: Implement me!
+	    (:td (str 561))
+	    (:td (str
+		  (if (connected-p source)
+		      "Connected"
+		      "Not Connected")))
+	    (:td
+	     (:a :href (format nil "/manage-sources?action=remove&sid=~a"
+			       (pointer-address (get-ptr source)))
+		 "Remove")
+	     " "
+	     (:a :href (format nil "/manage-sources?action=connect&id=~a"
+			       (pointer-address (get-ptr source)))
+		 (str
+		  (if (connected-p source)
+		      "Disconnect"
+		      "Connect"))))))))))))
