177b43bf007cff1f8659e700f4d0f9e8d65b0a89
Harry Bock <harry@oshean.org>
2009-07-08 13:14:27 -0400

    Show error messages in /config.
    Error messages now show up in the control panel when specified, and at the location where
    the error occured.

--- lisp/web-config.lisp
+++ lisp/web-config.lisp
@@ -66,67 +66,87 @@
 ;;;  - "networkparse": error parsing network string
 ;;;  - "missingvlan": bad VLAN when adding
 ;;;  - "editvlan": error editing VLAN IDs
-(hunchentoot:define-easy-handler (config :uri "/config") ((err :parameter-type 'integer))
+;;;  - "badvid": no parseable VID when editing VLANs.
+;;;  - "novname": no Name specified when editing VLANs.
+(hunchentoot:define-easy-handler (config :uri "/config") (error)
   (with-periscope-page ("Control Panel" :login t)
-    (unless *collector*
-      (warning-box
-       "Collector not initialized. This is a bug.")
-      (return-from config))
+    (flet ((error-message (message &key (table t))
+	     (if table
+		 (htm (:tr (:td :class "error" :colspan 3 (:b (str message)))))
+		 (htm (:b :class "error" (str message))))))
+      (unless *collector*
+	(warning-box
+	 "Collector not initialized. This is a bug.")
+	(return-from config))
 
-    (when err
-      (case err
-	(#.+config-success+
-	 (htm (:p "Configuration values successfully applied!")))))
+      (when (string= error "success")
+	(htm (:p "Configuration values successfully applied!")))
 
-    (with-config-form ("/set-config" "Monitoring Configuration" "monitor")
-      (:table
-       (:tr
-	(:td "Traffic Filter")
-	(:td (input "filter" (if *collector* (filter *collector*) "")))))
-      (:input :type "submit" :value "Apply Configuration"))
+      (with-config-form ("/set-config" "Monitoring Configuration" "monitor")
+	(:table
+	 (:tr
+	  (:td "Traffic Filter")
+	  (:td (input "filter" (if *collector* (filter *collector*) "")))))
+	(:input :type "submit" :value "Apply Configuration"))
 
-    (with-config-form ("/set-config" "Network Configuration" "network")
-      (:table
-       (:tr
-	(:td "Web interface port")
-	(:td (input "web-port" *web-port*)))
-       (:tr
-	(:td "Local Network (CIDR)")
-	(:td (input "network" (ip-string *internal-network* *internal-netmask*))))
-       (:tr
-	(:td "Notable ports (select to remove)")
-	(:td
-	 (:select
-	  :name "remove" :multiple t 
-	  (dolist (port *notable-ports*)
-	    (htm (:option :value port (fmt "~d (~a)" port (service-name port))))))))
-       (:tr
-	(:td "Add notable ports:")
-	(:td (input "ports" ""))))
-      (:input :type "submit" :value "Apply Configuration"))
+      (with-config-form ("/set-config" "Network Configuration" "network")
+	(:table
+	 (:tr
+	  (:td "Web interface port")
+	  (:td (input "web-port" *web-port*)))
+	 (cond
+	   ((string= error "nocidrsuffix")
+	    (error-message "Error: Network subnet mask must be specified (e.g., 192.168.10.0/24)."))
+	   ((string= error "networkparse")
+	    (error-message "Error parsing CIDR network specification.")))
+	 (:tr
+	  (:td "Local Network (CIDR)")
+	  (:td (input "network" (ip-string *internal-network* *internal-netmask*))))
+	 (:tr
+	  (:td "Notable ports (select to remove)")
+	  (:td
+	   (:select
+	    :name "remove" :multiple t 
+	    (dolist (port *notable-ports*)
+	      (htm (:option :value port (fmt "~d (~a)" port (service-name port))))))))
+	 (when (string= error "ports")
+	   (error-message "Error: Port numbers must be separated by spaces or commas."))
+	 (:tr
+	  (:td "Add notable ports:")
+	  (:td (input "ports" ""))))
+	(:input :type "submit" :value "Apply Configuration"))
 
-    (with-config-form ("/set-config" "Add VLAN Identifier" "addvlan")
-      (:table
-       (:tr
-	(:td "VLAN ID")
-	(:td (input "newvid" "")))
-       (:tr
-	(:td "VLAN Name")
-	(:td (input "newvname" ""))))
-      (:input :type "submit" :value "Add VLAN"))
+      (with-config-form ("/set-config" "Add VLAN Identifier" "addvlan")
+	(when (string= error "missingvlan")
+	  (error-message "Error setting VLAN identifier; both a valid VID and non-empty name
+must be specified!" :table nil))
+	(:table	 
+	 (:tr
+	  (:td "VLAN ID")
+	  (:td (input "newvid" "")))
+	 (:tr
+	  (:td "VLAN Name")
+	  (:td (input "newvname" ""))))
+	(:input :type "submit" :value "Add VLAN"))
 
-    (with-config-form ("/set-config" "Edit VLAN Identifiers" "editvlan")
-      (:table
-       :class "input"
-       (:tr (:th "VLAN ID") (:th "Name") (:th "Remove"))
-       (loop :with index = 0
-	  :for (vid name) :in (vlan-name-list) :do
-	  (htm (:tr
-		(:td (input (format nil "vid[~d]" index) vid :size 4))
-		(:td (input (format nil "vname[~d]" index) name))
-		(:td (checkbox (format nil "delete[~d]" index) :value vid))))
-	  (incf index)))
-      (:input :type "submit" :value "Commit Changes"))))
+      (with-config-form ("/set-config" "Edit VLAN Identifiers" "editvlan")
+	(cond
+	  ((string= error "badvid")
+	   (error-message "Bad VLAN ID; must be a positive integer between 0-4095." :table nil))
+	  ((string= error "novname")
+	   (error-message "VLAN names must not be empty. To delete an ID, please use the
+\"Remove\" checkbox." :table nil)))
+	(:table
+	 :class "input"
+	 (:tr (:th "VLAN ID") (:th "Name") (:th "Remove"))
+	 (loop :with index = 0
+	    :for (vid name) :in (vlan-name-list) :do
+	    (htm (:tr
+		  (:td (input (format nil "vid[~d]" index) vid :size 4))
+		  (:td (input (format nil "vname[~d]" index) name))
+		  (:td (checkbox (format nil "delete[~d]" index) :value vid))))
+	    (incf index)))
+	(:input :type "submit" :value "Commit Changes")))))
 
 (defun ports-from-string (port-string)
   "Take a string of port numbers, separated by spaces and/or commas, and return a sorted list
@@ -165,7 +185,7 @@ of integers corresponding to these numbers.  Duplicate and invalid port numbers 
 	     (setf *notable-ports*
 		   (delete-if (lambda (port)
 				(find port remove-list)) *notable-ports*)))
-
+	   
 	   (when (not (empty-string-p ports))
 	     (if (ppcre:scan "^(\\d{1,5}( *|(, *)))+$" ports)
 		 (setf *notable-ports*
