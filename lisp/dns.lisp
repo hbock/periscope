056449096b1da948197e765347ec238864355ccb
Harry Bock <harry@oshean.org>
2009-07-13 13:13:11 -0400

    Implement a much-improved DNS reverse lookup architecture.
    DNS requests are pooled into a queue, where they are processed asynchronously in another
    thread.  The interface to the user is the function HOSTNAME, which returns two values -
    the canonical hostname of the supplied IP address -  if it exists and has been determined -
    and a keyword indicating the status of the lookup, which is one of :FOUND, :NXDOMAIN, or
    :PROCESSING.  The application can do what it likes with these identifiers.  If the hostname
    has not or cannot be determined, the result of the function IP-STRING applied to the
    input parameter is returned along with its corresponding status (either :NXDOMAIN or :PROCESSING).

--- lisp/dns.lisp
+++ lisp/dns.lisp
@@ -18,6 +18,14 @@
 ;;;; Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 (in-package :periscope)
 
+(defvar *dns-thread*)
+(defvar *dns-lock* (bt:make-lock "dns"))
+(defvar *dns-cond* (bt:make-condition-variable))
+
+(defvar *dns-requests* nil)
+(defvar *dns-available-p* t)
+(defvar *dns-cache* (make-hash-table :size 10000))
+
 (defun ip-to-vector (ip)
   "Convert an integer IP address to a 4-element vector as required by SB-BSD-SOCKETS."
   (declare (type (unsigned-byte 32) ip))
@@ -27,9 +35,28 @@
    (ldb (byte 8  8) ip)
    (ldb (byte 8  0) ip)))
 
-(defun hostname (ip)
-  "Given an IP address, lookup the hostname of the corresponding machine, if available.
-Returns an empty string on lookup failure."
+(defun start-dns ()
+  (setf *dns-available-p* t)
+  (setf *dns-thread* (bt:make-thread #'dns-thread :name "DNS Reverse Lookups")))
+
+(defun stop-dns (&key (join t))
+  (setf *dns-available-p* nil)
+  (when join
+    (bt:join-thread *dns-thread*)))
+
+(defun dns-thread ()
+  (loop :while *dns-available-p* :do
+     (let ((host
+	    (bt:with-lock-held (*dns-lock*)
+	      (loop :while (null *dns-requests*) :do
+		 (bt:condition-wait *dns-cond* *dns-lock*))
+	      (setf *dns-requests* (remove-duplicates *dns-requests*))
+	      (pop *dns-requests*))))
+       (let ((hostname (reverse-lookup host)))
+	 (bt:with-lock-held (*dns-lock*)
+	   (setf (gethash host *dns-cache*) hostname))))))
+
+(defun reverse-lookup (ip)
   (if (broadcast-address-p ip)
       "Broadcast"
       #+sbcl
@@ -37,6 +64,23 @@ Returns an empty string on lookup failure."
 	  (sb-bsd-sockets:host-ent-name
 	   (sb-bsd-sockets:get-host-by-address (ip-to-vector ip)))
 	(sb-bsd-sockets:name-service-error (e)
-	  (declare (ignore e))
-	  ""))
+	  (declare (ignore e)) nil))
       #-sbcl nil))
+
+(defun hostname (ip)
+  "Given an IP address, lookup the hostname of the corresponding machine, if available.
+Returns the IP address as a string on lookup failure."
+  (cond
+    (*dns-available-p*
+     (bt:with-lock-held (*dns-lock*)
+       (multiple-value-bind (hostname existp)
+	   (gethash ip *dns-cache*)
+	 (if existp
+	     (if hostname
+		 (values hostname :found)
+		 (values (ip-string ip) :nxdomain))
+	     (progn
+	       (push ip *dns-requests*)
+	       (bt:condition-notify *dns-cond*)
+	       (values (ip-string ip) :processing))))))
+    (t (values (ip-string ip) :unavailable))))
