b30d6969e92f8c6f8db2bc560ee34553e216e190
Harry Bock <harry@oshean.org>
2009-07-22 14:00:08 -0400

    Finish basic time step functions.
    NEXT-HALF-HOUR
    NEXT-HOUR
    NEXT-DAY
    NEXT-WEEK
    NEXT-MONTH
    
    All working for both UNIVERSAL-TIME and TIMESTAMP objects.

--- lisp/time.lisp
+++ lisp/time.lisp
@@ -21,12 +21,16 @@
 (deftype universal-time () '(integer 536870912))
 (defparameter +month-days+
     #(0 31 28 31 30 31 30 31 31 30 31 30 31))
+(defparameter *default-offset* -18000)
 
 (defun leap-year-p (year)
   "Returns true if year is a leap year."
   (and (zerop (mod year 4))
        (or (zerop (mod year 400))
            (not (zerop (mod year 100))))))
+
+(defun minimize-part (time part)
+  (timestamp-minimize-part time part :offset *default-offset*))
 
 (defun month-days (month year)
   (declare (type (integer 1 12) month))
@@ -39,16 +43,29 @@
 
 (defun normalize-day (day month year)
   (let ((month-days (month-days month year)))
-    (if (> day month-days) 1 day)))
+    (if (> day month-days) (mod day month-days) day)))
 
 (defun normalize-month (month)
-  (if (> month 12) 1 month))
+  (if (> month 12) (mod month 12) month))
 
 (defmethod next-half-hour ((time timestamp))
-  (let ((min-hour (timestamp-minimize-part time :min)))
-    (if (>= (timestamp-minute time) 30)
-	(timestamp+ min-hour 1 :hour)
-	(timestamp+ min-hour 30 :minute))))
+  (universal-to-timestamp (next-half-hour (timestamp-to-universal time)))
+  ;; (let ((min-hour (minimize-part time :min)))
+  ;;   (if (>= (timestamp-minute time) 30)
+  ;; 	(timestamp+ min-hour 1 :hour)
+  ;; 	(timestamp+ min-hour 30 :minute)))
+  )
+
+(defmethod next-half-hour ((time integer))
+  (multiple-value-bind (sec min hour date month year day)
+      (decode-universal-time time)
+    (declare (ignore sec day))
+    (let* ((min (if (>= min 30) 0 30))
+	   (hour (normalize-hour (if (zerop min) (1+ hour) hour)))
+	   (new-date (normalize-day (if (zerop hour) (1+ date) date) month year))
+	   (new-month (normalize-month (if (< new-date date) (1+ month) month)))
+	   (new-year (if (< new-month month) (1+ year) year)))
+      (encode-universal-time 0 min hour new-date new-month new-year))))
 
 (defmethod next-hour ((time integer))
   (multiple-value-bind (sec min hour date month year day)
@@ -69,25 +86,46 @@
 	   (new-year (if (< new-month month) (1+ year) year)))
       (encode-universal-time 0 0 0 new-date new-month new-year))))
 
+(defmethod next-week ((time integer))
+  (multiple-value-bind (sec min hour date month year day)
+      (decode-universal-time time)
+    (declare (ignore sec min hour))
+    (let* ((new-date (normalize-day (+ date (if (= 6 day) 7 (- 6 day))) month year))
+	   (new-month (normalize-month (if (< new-date date) (1+ month) month)))
+	   (new-year (if (< new-month month) (1+ year) year)))
+      (encode-universal-time 0 0 0 new-date new-month new-year))))
+
+(defmethod next-month ((time integer))
+  (multiple-value-bind (sec min hour date month year day)
+      (decode-universal-time time)
+    (declare (ignore sec min hour date day))
+    (let* ((new-month (normalize-month (1+ month)))
+	   (new-year (if (< new-month month) (1+ year) year)))
+      (encode-universal-time 0 0 0 1 new-month new-year))))
+
 (defmethod next-hour ((time timestamp))
-  (timestamp+ (timestamp-minimize-part time :min) 1 :hour))
+  ;(timestamp+ (minimize-part time :min) 1 :hour)
+  (universal-to-timestamp (next-hour (timestamp-to-universal time))))
 
 (defmethod next-day ((time timestamp))
-  (timestamp+ (timestamp-minimize-part time :hour) 24 :hour))
+  ;(timestamp+ (timestamp-minimize-part time :hour) 1 :day)
+  (universal-to-timestamp (next-day (timestamp-to-universal time))))
 
-(defun next-week (time)
-  (let ((this-sunday
-	 (timestamp-minimize-part
-	  ;; ADJUST-TIMESTAMP will always return the previous Sunday, but we specifically
-	  ;; want THIS Sunday - thus, if 'time' is already on a Sunday, we just minimize the
-	  ;; hours on time as-is.
-	  (if (zerop (timestamp-day-of-week time))
-	      time
-	      (adjust-timestamp time (offset :day-of-week :sunday))) :hour)))
-    (timestamp+ this-sunday 7 :day)))
+(defmethod next-week ((time timestamp))
+  ;; (let ((this-sunday
+  ;; 	 (minimize-part
+  ;; 	  ;; ADJUST-TIMESTAMP will always return the previous Sunday, but we specifically
+  ;; 	  ;; want THIS Sunday - thus, if 'time' is already on a Sunday, we just minimize the
+  ;; 	  ;; hours on time as-is.
+  ;; 	  (if (zerop (timestamp-day-of-week time))
+  ;; 	      time
+  ;; 	      (adjust-timestamp time (offset :day-of-week :sunday))) :hour)))
+  ;;   (timestamp+ this-sunday 7 :day))
+  (universal-to-timestamp (next-week (timestamp-to-universal time))))
 
-(defun next-month (time)
-  (timestamp+ (timestamp-minimize-part time :day) 1 :month))
+(defmethod next-month ((time timestamp))
+  ;(timestamp+ (timestamp-minimize-part time :day :offset -18000) 1 :month)
+  (universal-to-timestamp (next-month (timestamp-to-universal time))))
 
 (defun time-split (flow-sequence next-time-fun)
   (flet ((%time-split (flow-sequence timestamp)
