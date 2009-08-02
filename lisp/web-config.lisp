5cadcb5e117f81618dcd90918120d879ed99ef35
Harry Bock <harry@oshean.org>
2009-08-01 20:54:08 -0400

    Implement configuration support for multiple internal networks.
    It is now possible to specify a list of internal networks in /config.

--- lisp/web-config.lisp
+++ lisp/web-config.lisp
@@ -58,15 +58,16 @@
       (:table
        (:tr
 	(:td "Traffic Filter")
-	(:td (input "filter" (if *collector* (filter *collector*) ""))))
+	(:td (input "filter" (if *collector* (filter *collector*) "") :size 30)))
        (cond
 	 ((string= error "nocidrsuffix")
 	  (error-message "Error: Network subnet mask must be specified (e.g., 192.168.10.0/24)."))
 	 ((string= error "networkparse")
 	  (error-message "Error parsing CIDR network specification.")))
        (:tr
-	(:td "Local Network (CIDR)")
-	(:td (input "network" (ip-string *internal-network* *internal-netmask*))))
+	(:td "Local Networks (CIDR)")
+	(:td (input "network" (format nil "~{~a~^, ~}" (network-strings *internal-networks*))
+		    :size 30)))
        (:tr
 	(:td "Notable ports (select to remove)")
 	(:td
@@ -119,6 +120,17 @@ must be specified!" :table nil))
 of integers corresponding to these numbers.  Duplicate and invalid port numbers are removed."
   (parse-integer-list port-string "\\d{1,5}" (lambda (port) (> port 65535))))
 
+(defun subnets-from-string (subnet-string)
+  "Take a string of CIDR subnet specifications, separated by spaces and/or commas, and return 
+a list of networks and netmasks corresponding to these specifications.  Each network and netmask
+combination form a dotted list, with the CAR representing the network and the CDR the netmask.
+Invalid CIDR subnets will signal a PARSE-ERROR."
+  (loop :for subnet :in
+     (tokenize subnet-string (list #\Space #\Tab #\,)) :collect
+     (multiple-value-bind (network netmask)
+	 (parse-ip-string subnet)
+       (cons network netmask))))
+
 (hunchentoot:define-easy-handler (set-config :uri "/set-config")
     (action (web-port :parameter-type 'integer) dnslookup
 	    network ports filter
@@ -170,12 +182,7 @@ of integers corresponding to these numbers.  Duplicate and invalid port numbers 
 	   
        (when network
 	 (handler-case
-	     (multiple-value-bind (network netmask)
-		 (parse-ip-string network)
-	       (unless netmask
-		 (error-redirect "nocidrsuffix"))
-	       (setf *internal-network* network)
-	       (setf *internal-netmask* netmask))
+	     (setf *internal-networks* (subnets-from-string network))
 	   (parse-error ()
 	     (error-redirect "networkparse")))))
 
