441012c72f4992525a96a0f0091635ff2421cc3a
Harry Bock <harry@oshean.org>
2009-08-02 21:04:19 -0400

    Service cache hash table doesn't need :test #'equal.
    Overkill for port numbers.

--- lisp/specials.lisp
+++ lisp/specials.lisp
@@ -59,7 +59,7 @@
 
 (defvar *notable-ports* (list 22 53 80 443 51413))
 
-(defvar *service-cache* (make-hash-table :test #'equal :size 500))
+(defvar *service-cache* (make-hash-table :size 500))
 (defvar *vlan-names* (make-hash-table))
 
 (defvar *internal-network* #x0a000000)
