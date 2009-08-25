82155518971a0699a4255842d3d30aca527098ba
Harry Bock <harry@oshean.org>
2009-08-25 13:08:47 -0400

    Add connection support to /db-test test page; remove old code.
    Remove stale, crufty code that wouldn't compile now anyway.

--- lisp/test.lisp
+++ lisp/test.lisp
@@ -20,54 +20,7 @@
 
 (define-report-handler (db-report-test "/db-test" "Database Report Test") ()
   (with-periscope-page ("Database Test")
+    (when (not (database-connected-p))
+      (htm (:b "Not connected, reconnecting..."))
+      (database-connect "periscope"))
     (:div :class "stats" (print-html *current-report*))))
-
-(define-report-handler (split-test "/time-split" "Time Split Test") ()
-  (with-periscope-page ("TIME-SPLIT test")
-    (when *flow-list*
-      (let* ((time-list
-	      (time-split *flow-list* #'next-hour))
-	     (reports
-	      (mapcar
-	       (lambda (list)
-		 (make-instance 'periodic-report :flow-list list :time
-				(this-hour (end-time (first list)))))
-	       time-list)))
-	(htm
-	 (:div
-	  :class "stats"
-	  (loop
-	     for i from 0 upto (min 5 (length time-list))
-	     for report in reports do
-	     (print-html report)
-	     (htm
-	      (:table
-	       (:tr (:th :colspan 4 "Source") (:th :colspan 4 "Destination")
-		    (:th :colspan 3 "Flow information"))
-	       (:tr (:th "IP") (:th "Port") (:th "Packets") (:th "VLAN")
-		    (:th "IP") (:th "Port") (:th "Packets") (:th "VLAN")
-		    (:th "Protocol") (:th "First Seen") (:th "Last Seen"))
-	       (loop :for flow :in (nth i time-list) :repeat 100 :do
-		  (print-html flow)))))))))))
-
-(define-report-handler (test "/vlan-test" "VLAN Filter Test") ()
-  (with-periscope-page ("VLAN Filter Test")
-    (:h2 "VLAN Filter Test")
-    (when *flow-list*
-      (htm
-       (:div
-	:class "stats"
-	(loop :for report :in (make-filtered-reports *flow-list* (user))
-	   :do (print-html report)))))))
-
-(defun print-flows (flow-list &optional (limit 100))
-  (with-html-output (*standard-output*)
-    (:table
-     (:tr (:th :colspan 4 "Source")
-	  (:th :colspan 4 "Destination")
-	  (:th :colspan 3 "Flow information"))
-     (:tr (:th "IP") (:th "Port") (:th "Packets") (:th "VLAN")
-	  (:th "IP") (:th "Port") (:th "Packets") (:th "VLAN")
-	  (:th "Protocol") (:th "First Seen") (:th "Last Seen"))
-     (loop :for flow :in flow-list :repeat limit :do
-	(print-html flow)))))
